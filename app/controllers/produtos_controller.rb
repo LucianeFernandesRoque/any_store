63a7089243566734eea2de2fa22c5819a67c50bc
Luciane Fernandes <luciane.fernandes@rebase.com.br>
2021-06-30 17:24:34 -0300

    Create pages products

--- app/controllers/produtos_controller.rb
+++ app/controllers/produtos_controller.rb
@@ -1,7 +1,11 @@
 class ProdutosController < ApplicationController
-
   def index
     @produtos_preco = Produto.order :preco
     @produtos_nome = Produto.order(:nome).limit 5
+  end
+
+  def create
+    valores = params.require(:produto).permit :nome, :preco, :descricao, :quantidade
+    produto = Produto.create  valores
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,5 @@
 Rails.application.routes.draw do
-  get 'produtos' => 'produtos#index'
+  post '/produtos' => 'produtos#create'
+  get '/produtos/new' => 'produtos#new'
+  root 'produtos#index'
 end

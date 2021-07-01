fb0f8a9a7cf6ef34ac8b1bcc7132a22441ed94c7
Luciane Fernandes <luciane.fernandes@rebase.com.br>
2021-07-01 13:51:27 -0300

    implemented the destroy method :wastebasket:

--- app/controllers/produtos_controller.rb
+++ app/controllers/produtos_controller.rb
@@ -7,5 +7,14 @@ class ProdutosController < ApplicationController
   def create
     valores = params.require(:produto).permit :nome, :preco, :descricao, :quantidade
     produto = Produto.create  valores
+    redirect_to root_url
+
+  end
+
+  def destroy
+    id = params[:id]
+    Produto.destroy id
+    redirect_to root_url
+
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
   post '/produtos' => 'produtos#create'
   get '/produtos/new' => 'produtos#new'
+  delete '/produtos/:id' => "produtos#destroy", as: :produto
   root 'produtos#index'
+
 end
